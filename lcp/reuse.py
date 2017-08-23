1d790cce3827139885d319ba093b09ac180cef1f
Tom Pollard <tpollard@mit.edu>
2017-08-23 12:30:16 -0400

    extract list from pubmed with keyword

--- lcp/reuse.py
+++ lcp/reuse.py
@@ -2,6 +2,8 @@
 Utilities for tracking reuse of LCP resources
 """
 
+from Bio import Entrez
+
 def helloworld():
 
     a = ['h','l','o','o','l']
@@ -13,5 +15,34 @@ def helloworld():
     
     return c
 
+def search(query, email, db='pubmed', sort='relevance', retmax='20', 
+            retmode='xml'):
+    """
+    Search for relevant publications.
+    """
+    # email is required as point of contact for Entrez
+    Entrez.email = email
 
+    # add search critera
+    handle = Entrez.esearch(db=db, 
+                            sort=sort, 
+                            retmax=retmax,
+                            retmode=retmode, 
+                            term=query)
+    # fetch the results
+    results = Entrez.read(handle)
+    return results
 
+def fetch_details(id_list, email, db='pubmed', retmode='xml'):
+    """
+    Fetch details of articles using the search results
+    """
+    # email is required as point of contact for Entrez
+    Entrez.email = email
+
+    # fetch the results
+    ids = ','.join(id_list)
+    handle = Entrez.efetch(db=db, retmode=retmode, id=ids)
+    results = Entrez.read(handle)
+    
+    return results
